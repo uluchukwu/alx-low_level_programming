d6fb64006b94a68420742969bfc0c2758e6c7e80
Sopuluchukwu <urealsean12@gmail.com>
2023-04-30 03:30:13 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of strings containing arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	puts(argv[0]);
+	return 0;
+}
